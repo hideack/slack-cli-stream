9a3dd8989557331c01313a530ca6f17b83595f4d
hideack <hide.terai@gmail.com>
2017-05-21 10:07:07 +0900

    requireするファイル名の修正とテスト対象の関数を修正

--- test/util_test.js
+++ test/util_test.js
@@ -1,5 +1,5 @@
 let assert = require("chai").assert;
-let util = require("../lib/util.js");
+let util = require("../lib/utility.js");
 
 describe("ユーティリティのテスト", () => {
   beforeEach( () => {
@@ -8,11 +8,11 @@ describe("ユーティリティのテスト", () => {
   });
 
   it("Slack message中の1ユーザに対するメンションのユーザIDを置換できること", () => {
-    assert.equal(util.replaceId("<@U0XXYYZZ0> Hello."), "@alice Hello.", "Slack IDに置換できている");
+    assert.equal(util.replaceSlackId("<@U0XXYYZZ0> Hello."), "@alice Hello.", "Slack IDに置換できている");
   });
 
   it("Slack message中の2ユーザに対するメンションのユーザIDを置換できること", () => {
-    assert.equal(util.replaceId("<@U0XXYYZZ0> <@U0XXYYZZ1> Hello."), "@alice @bob Hello.", "Slack IDに置換できている");
+    assert.equal(util.replaceSlackId("<@U0XXYYZZ0> <@U0XXYYZZ1> Hello."), "@alice @bob Hello.", "Slack IDに置換できている");
   });
 
 });
